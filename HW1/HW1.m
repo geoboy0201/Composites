084a08d0abca5604df7b93e21e4196af727b0434
Chris Hackbardt <113145812+geoboy0201@users.noreply.github.com>
2023-01-17 23:03:38 -0500

    Create HW1.m

---
+++ HW1/HW1.m
@@ -0,0 +1,18 @@
+clc;clear;
+length1=0.01;
+dia1=0.01;
+volume=(pi*(dia1/2)^2)*length1;
+SA1=(pi*dia1*length1)+(2*pi*(dia1/2)^2);
+SAVratio=SA1/volume;
+diameters=1e-9:0.01:1;
+ARs=[];
+SAVratios=[];
+for i=1:length(diameters)
+    len=volume/(pi*(diameters(i)/2)^2);
+    ARs(i)=len/diameters(1);
+    SA=(pi*diameters(1)*len)+(2*pi*(diameters(1)/2)^2);
+    SAVratios(i)=SA/volume;
+end
+plot(ARs,SAVratios)
+xlabel('Aspect Ratio')
+ylabel('Surface Area to Volume Ratio')
