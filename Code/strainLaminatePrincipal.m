b687046ea5aaae10ecfa28b385c2480267fb1e5f
Chris Hackbardt <113145812+geoboy0201@users.noreply.github.com>
2023-03-01 21:24:46 -0500

    HW6

--- Code/strainLaminatePrincipal.m
+++ Code/strainLaminatePrincipal.m
@@ -4,13 +4,13 @@ function [eps1,eps2,gamma12] = strainLaminatePrincipal(epsx,epsy,gammaxy,thetas)
     eps2=zeros(1,N);
     gamma12=zeros(1,N);
     for i=2:2:N
-        strainTrans=strainTransformationMatrix(thetas(N/2));
-        principleStrains=strainTrans*[epsx(N);epsy(N);gammaxy(N)];
-        eps1(N)=principleStrains(1);
-        eps1(N-1)=eps1(N);
-        eps2(N)=principleStrains(2);
-        eps2(N-1)=eps2(N);
-        gamma12(N)=principleStrains(3);
-        gamma12(N-1)=gamma12(N);
+        strainTrans=strainTransformationMatrix(thetas(i/2));
+        principleStrains=strainTrans*[epsx(i);epsy(i);gammaxy(i)];
+        eps1(i)=principleStrains(1);
+        eps1(i-1)=eps1(i);
+        eps2(i)=principleStrains(2);
+        eps2(i-1)=eps2(i);
+        gamma12(i)=principleStrains(3);
+        gamma12(i-1)=gamma12(i);
     end
 end
--- HW6/test.m
+++ HW6/test.m
@@ -9,7 +9,23 @@ distances=laminateDistances(thicknesses);
 
 [epsx0,epsy0,gammaxy0,kx,ky,kxy] = midPlaneStrainsCurvature(0,3500,0,0,0,0,thicknesses,thetas,E1s,E2s,G12s,v12s);
 [epsx,epsy,gammaxy] = strainLaminateStructural(epsx0,epsy0,gammaxy0,kx,ky,kxy,thicknesses);
-[eps1,eps2,gamma12] = strainLaminatePrincipal(epsx,epsy,gammaxy,thetas);
+
+N=length(epsx);
+eps1=zeros(1,N);
+eps2=zeros(1,N);
+gamma12=zeros(1,N);
+for i=2:2:N
+    strainTrans=strainTransformationMatrix(thetas(i/2));
+    principleStrains=strainTrans*[epsx(i);epsy(i);gammaxy(i)];
+    eps1(i)=principleStrains(1);
+    eps1(i-1)=eps1(i);
+    eps2(i)=principleStrains(2);
+    eps2(i-1)=eps2(i);
+    gamma12(i)=principleStrains(3);
+    gamma12(i-1)=gamma12(i);
+    fprintf('%g \n',thetas(i/2))
+    fprintf('%g \n',i)
+end
 
 figure
 plot(epsx,distances,'k-o',epsy,distances,'b-^',gammaxy,distances,'r-square')
