017ead6814162841b3fbaa34fa68c2f609202fd3
Chris Hackbardt <113145812+geoboy0201@users.noreply.github.com>
2023-04-26 15:04:20 -0400

    hw9

--- Code/compositePlateFailureMaxStress.m
+++ Code/compositePlateFailureMaxStress.m
@@ -1,19 +1,32 @@
-function [FS,Nxmax,Nymax,Nxymax,Mxmax,Mymax,Mxymax]=compositePlateFailureMaxStress(laminate,length,width,Px,Py,Pxy,Qx,Qy,Qxy)
+function [PxMax]=compositePlateFailureMaxStress(laminate,length,width)
 
-Nx=Px/width;
-Ny=Py/length;
-Nxy=Pxy;
-Mx=Qx/width;
-My=Qy/length;
-Mxy=Qxy;
+[~,~,~,~,~,~,longStrengthTen,longStrengthCom,tranStrengthTen,tranStrengthCom,strengthLT,~,~,~,~,~,~] = laminateReader(laminate);
 
-FS=maximumStressTheory(laminate,Nx,Ny,Nxy,Mx,My,Mxy);
 
-Nxmax=Nx/FS;
-Nymax=Ny/FS;
-Nxymax=Nxy/FS;
-Mxmax=Mx/FS;
-Mymax=My/FS;
-Mxymax=Mxy/FS;
+Nx=1/width;
+Ny=0;
+Nxy=0;
+Mx=0;
+My=0;
+Mxy=0;
+
+[~,~,~,sigma1,sigma2,tau12,~,~,~,~,~,~]=forces2StressStrainLaminateNoPlot(laminate,Nx,Ny,Nxy,Mx,My,Mxy);
+
+for i=1:length(sigma1)
+    PxTen1(i)=longStrengthTen/sigma1(i);
+    PxCom1(i)=longStrengthCom/sigma1(i);
+    PxTen2(i)=tranStrengthTen/sigma2(i);
+    PxCom2(i)=tranStrengthCom/sigma2(i);
+    Px12(i)=strengthLT/tau12(i);
+end
+
+PxTen1Max=min(PxTen1);
+PxCom1Max=min(PxCom1);
+PxTen2Max=min(PxTen2);
+PxCom2Max=min(PxCom2);
+Px12Max=min(Px12);
+
+PxMax=[PxTen1Max,PxCom1Max,PxTen2Max,PxCom2Max,Px12Max];
+PxMax=min(PxMax);
 
 end
